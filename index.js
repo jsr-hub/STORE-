0d90618d021c68505fe661af260797c41bdc0ab4
Sanjay_Raju <44097832+jsr-hub@users.noreply.github.com>
2020-04-05 22:30:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express"); 
+const bodyPars = require("body-parser"); // to parse post request body
+const cors = require("cors"); // accept cross client request
+let schema = require("./schema"); // 
+let {nstore} = schema;
+const app = express();
+i=0
+// using middle ware for parse and cross request
+app.use(bodyPars());
+// only allowing cros fom my site
+app.use( cors({
+    }));
+app.post("/sl",(req,res)=>{
+    const body = req.body;
+    nstore.findOne({brand:body.brand,product:body.product,unit:body.unit,price:body.price},(err,example)=>{
+    	if(example)
+    	{
+    		res.write("duplicate")
+    		res.end();
+    	}
+    	else
+    	{
+    		let val = new nstore({      // fitting request to menu schema
+	        brand:body.brand,
+	        product:body.product,
+	        price:body.price,
+	        unit:body.unit
+	    	});
+	    	val.save().then(doc=>{   // saving new item to  mongoose
+	        res.write("sucess");
+	        res.end();
+	    }) 
+    	}
+    })  // copying request body
+});
+app.post("/dl",(req,res)=>{
+    const body = req.body;
+    d=nstore.find({brand:body.brand,product:body.product,unit:body.unit,price:body.price}).remove().exec(); 
+});
+app.get("/gl",(req,res)=>{
+    nstore.find({},{__v:0,_id:0},(err,val)=>{ //request all data from menu collection
+        res.send(val)  
+    })
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log("server started lisening in port 3000.....");
+})
