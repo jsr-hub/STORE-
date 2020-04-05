ee3880d297741d29255cd6d021af3863f7d50ff8
Sanjay_Raju <44097832+jsr-hub@users.noreply.github.com>
2020-04-05 22:15:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,33 +3,7 @@ const bodyPars = require("body-parser"); // to parse post request body
 const cors = require("cors"); // accept cross client request
 const path = require('path')
 const multer = require("multer");
-//const upload = multer({dest:'uploads/'});
-const storage = multer.diskStorage({
-	destination: function(req, file, cb) {
-	  cb(null, './uploads');
-	},
-	filename: function(req, file, cb) {
-	  cb(null,  Date.now() +file.originalname);
-	}
-  });
-
-  const fileFilter = (req, file, cb) => {
-	// reject a file
-	if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-	  cb(null, true);
-	} else {
-	  cb(null, false);
-	}
-  };
-  const upload = multer({
-	storage: storage,
-	limits: {
-		fileSize: 1024 * 1024 * 5
-	  },
-	fileFilter: fileFilter
-  });
-
-
+const upload = multer({dest:'uploads/'});
 let schema = require("./schema"); // 
 let {nstore} = schema;
 const app = express();
